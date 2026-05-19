6b747876156d70a86c4d82dbdd553f3957ac4ef8
dieblume86 <drouot.remi@gmail.com>
2026-05-19 17:28:37 +0200

    [Update] Language selection switch fixed

--- P2FixAnAppDotNetCode/Models/Services/LanguageService.cs
+++ P2FixAnAppDotNetCode/Models/Services/LanguageService.cs
@@ -23,9 +23,23 @@ namespace P2FixAnAppDotNetCode.Models.Services
         public string SetCulture(string language)
         {
             string culture = "";
-            // TODO complete the code 
-            // Default language is "en", french is "fr" and spanish is "es".
-            
+
+            switch (language)
+            {
+                case "English":
+                    culture = "en";
+                    break;
+                case "French":
+                    culture = "fr";
+                    break;
+                case "Spanish":
+                    culture = "es";
+                    break;
+                default:
+                    culture = "en";
+                    break;
+            }
+
             return culture;
         }
 
